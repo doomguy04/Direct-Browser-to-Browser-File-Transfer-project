a38c54c3988d331c8ba12de86eec22c1f373b623
doomguy04 <vivv7856@gmail.com>
2026-06-14 17:14:58 +0530

    fix: expand STUN servers list in webrtc.js to improve cross-network NAT traversal

--- client/src/utils/webrtc.js
+++ client/src/utils/webrtc.js
@@ -4,7 +4,9 @@ const ICE_SERVERS = {
   iceServers: [
     { urls: 'stun:stun.l.google.com:19302' },
     { urls: 'stun:stun1.l.google.com:19302' },
-    { urls: 'stun:stun2.l.google.com:19302' }
+    { urls: 'stun:stun2.l.google.com:19302' },
+    { urls: 'stun:stun.services.mozilla.com' },
+    { urls: 'stun:stun.twilio.com:3478' }
   ]
 };
 
